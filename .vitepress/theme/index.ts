ba5c3bb57c5dec6ccfc256afe1f6203013f632f0
n1coc4cola <n1coc4cola@gmail.com>
2026-03-11 13:55:04 +0100

    Fix icon css import issues

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { initComponent } from 'vitepress-mermaid-preview/component'
 import 'vitepress-mermaid-preview/dist/index.css'
+import 'virtual:group-icons.css'
+import 'uno.css'
 import './style.css'
 
 export default {
